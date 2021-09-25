38140ffb999bd4dd58b84307a925444c6be6d511
dedrasher <78565796+dedrasher@users.noreply.github.com>
2021-09-25 19:02:55 +0300

    Add files via upload

---
+++ Scripts/Localization.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Localization : MonoBehaviour
+{
+    [SerializeField] private int id = -1;
+    private TextMeshProUGUI shared;
+    private bool isGame;
+    private LocalizeManager.ChangeLanguageDelegate changeLanguageDelegate;
+    private void Start()
+    {
+        isGame = SceneManager.GetActiveScene().buildIndex == 1;
+        shared = GetComponent<TextMeshProUGUI>();
+        if (!isGame)
+        {
+            changeLanguageDelegate = delegate
+            {
+                shared.text = LocalizeManager.GetLocalizedString(id, isGame);
+            };
+            if(LocalizeManager.CurrentLanguage == Language.Russian)
+            changeLanguageDelegate.Invoke();
+            LocalizeManager.AddChangeListener(changeLanguageDelegate);
+        }
+        else if(LocalizeManager.CurrentLanguage == Language.Russian)
+            shared.text = LocalizeManager.GetLocalizedString(id, isGame);
+    }
+    private void OnDestroy()
+    {
+        if (!isGame && !LocalizeManager.IsChangeListenersListClear)
+        {
+            LocalizeManager.ClearChangeListeners();
+        }
+     }
+}
