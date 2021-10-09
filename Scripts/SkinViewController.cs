aecb994122cec13a0f38f844200898a1d8d3d8f9
dedrasher <78565796+dedrasher@users.noreply.github.com>
2021-10-09 23:32:37 +0300

    Update SkinViewController.cs

--- Scripts/SkinViewController.cs
+++ Scripts/SkinViewController.cs
@@ -5,19 +5,18 @@ public class SkinViewController : MonoBehaviour
 {
     public static GameObject SharedGameObject { get; private set; }
     [SerializeField] private UIBehaviour acceptPanel;
-    private Transform holderTransform, currentTransform;
+    private Transform currentTransform;
     private Vector3 one;
     private void Awake()
     {
         one = Vector3.one;
         currentTransform = transform;
-        holderTransform = currentTransform.parent;
-        SharedGameObject = holderTransform.gameObject;
+        SharedGameObject = gameObject;
     }
     
     private void OnMouseDrag()
     {
-        if (Input.touchCount > 0 && holderTransform.localScale == one && !acceptPanel.IsActive())
+        if (Input.touchCount > 0 && currentTransform.localScale == one && !acceptPanel.IsActive())
             currentTransform.Rotate(Input.touches.Last().deltaPosition.y * 0.3f,0f,0f);   
     }
     
